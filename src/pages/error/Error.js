e37058d1fe8659660aa792115395b68ba841d751
Bishal Moktan <busial.mok9@gmail.com>
2023-01-08 23:46:01 +0530

    Style error page and update meta tags

--- src/pages/error/Error.js
+++ src/pages/error/Error.js
@@ -1,6 +1,13 @@
+import { Link } from "react-router-dom"
+import "./error.css"
+
+
 const Error = () => {
   return (
-    <div>Error</div>
+    <div className="error">
+      <h1>Oops! no search found</h1>
+      <Link className="link" to="/">Back Home</Link>
+    </div>
   )
 }
 export default Error
